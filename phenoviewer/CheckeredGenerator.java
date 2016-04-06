547166a2ea9e381c27ea46c15606f68f89cf0a01
Héctor Castelli Zacharias <hectorxss@gmail.com>
2016-04-06 17:21:45 -0300

    Added checkered generator

--- phenoviewer/CheckeredGenerator.java
+++ phenoviewer/CheckeredGenerator.java
@@ -12,22 +12,22 @@ import java.io.File;
 import java.io.IOException;
 
 public class CheckeredGenerator extends JFrame {
-  public CheckeredGenerator(BufferedImage image, BufferedImage mask) {
+  public CheckeredGenerator(final BufferedImage image) {
     super("Square Tiles Generator");
     this.setSize(500,110);
-    JPanel panel = new JPanel();
+    final JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(0,2));
-    JTextField x = new JTextField("1");
-    JTextField y = new JTextField("1");
+    final JTextField x = new JTextField("1");
+    final JTextField y = new JTextField("1");
 
     JButton divide = new JButton("Okay");
     divide.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        setSize(800,800);
+        setSize(800,700);
         remove(panel);
         //Show Image with squared image on top and selector
         JPanel imagePanel = new JPanel();
-        imagePanel.setLayout(new GridLayout(0,1));
+        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.PAGE_AXIS));
         imagePanel.add(new JLabel("Select the squares you want the software to analyze:"));
 
         BufferedImage squared = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
@@ -39,15 +39,42 @@ public class CheckeredGenerator extends JFrame {
         BasicStroke bs = new BasicStroke(2);
         g2d.setStroke(bs);
         // draw the black vertical and horizontal lines
-        for(int i=0;i<Integer.parseInt(x.getText());i++){
-            // unless divided by some factor, these lines were being
-            // drawn outside the bound of the image..
-            g2d.drawLine((image.getWidth()+2)/20*i, 0, (image.getWidth()+2)/20*i,image.getHeight()-1);
-            g2d.drawLine(0, (image.getHeight()+2)/20*i, image.getWidth()-1, (image.getHeight()+2)/20*i);
+        int xint = Integer.parseInt(x.getText());
+        int yint = Integer.parseInt(y.getText());
+        /*for(int i=0;i<xint;i++){
+          // unless divided by some factor, these lines were being
+          // drawn outside the bound of the image..
+          int x1 = (image.getWidth()+1)*i/xint;
+          g2d.drawLine(x1, 0, x1,image.getHeight());
         }
-        imagePanel.add(new JLabel(new ImageIcon(image.getScaledInstance(800, 400, Image.SCALE_DEFAULT))));
+        for(int i=0;i<yint;i++){
+          int y1 = (image.getHeight()+1)*i/yint;
+          g2d.drawLine(0, y1, image.getWidth(), y1);
+        }
+        imagePanel.add(new JLabel(new ImageIcon(image.getScaledInstance(800, 400, Image.SCALE_SMOOTH))));
 
+        JFrame temp = new JFrame();
+        temp.add(new JLabel(new ImageIcon(image)));
+        temp.setSize(image.getWidth(),image.getHeight());
+        temp.setVisible(true);
+        */
+        JPanel miniatures = new JPanel();
+        miniatures.setLayout(new GridLayout(yint, xint, 3, 3)); //3 is value of gaps between tiles
+        for (int j=0;j<yint;j++) {
+          for(int i=0;i<xint;i++) {
+            int x1,y1,h1,w1;
+            x1=i*image.getWidth()/xint;
+            y1=j*image.getHeight()/yint;
+            w1=image.getWidth()/xint;
+            h1=image.getHeight()/yint;
 
+            JLabel section = new JLabel(new ImageIcon(image.getSubimage(x1,y1,w1,h1).getScaledInstance(800/xint,600/xint,Image.SCALE_SMOOTH)));
+            miniatures.add(section);
+            //
+          }
+        }
+        imagePanel.add(miniatures);
+        imagePanel.add(new JLabel(""));
         add(imagePanel);
       }
     });
@@ -65,4 +92,3 @@ public class CheckeredGenerator extends JFrame {
     this.add(panel);
   }
 }
-

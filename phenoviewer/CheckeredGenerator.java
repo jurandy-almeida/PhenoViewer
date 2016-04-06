e233585c3eeb04686a94961f285ce4e9c67805f6
Héctor Castelli Zacharias <hectorxss@gmail.com>
2016-04-06 12:23:28 -0300

    Added image

--- phenoviewer/CheckeredGenerator.java
+++ phenoviewer/CheckeredGenerator.java
@@ -20,12 +20,33 @@ public class CheckeredGenerator extends JFrame {
     JTextField x = new JTextField("1");
     JTextField y = new JTextField("1");
 
+    JButton divide = new JButton("Okay");
+    divide.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setSize(800,800);
+        remove(panel);
+        //Show Image with squared image on top and selector
+        JPanel imagePanel = new JPanel();
+        imagePanel.setLayout(new GridLayout(0,1));
+        imagePanel.add(new JLabel("Select the squares you want the software to analyze:"));
+        imagePanel.add(new JLabel(new ImageIcon(image.getScaledInstance(800, 400, Image.SCALE_DEFAULT))));
+
+
+
+        add(imagePanel);
+      }
+    });
+
+
     panel.add(new JLabel("Número de quadrados:"));
     panel.add(new JLabel(""));
     panel.add(new JLabel("Horizontais:"));
     panel.add(new JLabel("Verticais"));
     panel.add(x);
     panel.add(y);
+    panel.add(divide);
+
+
     this.add(panel);
   }
 }
